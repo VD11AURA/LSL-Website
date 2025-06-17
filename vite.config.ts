b26cfec90fc61c8a0e739a15f8721a785dcfae33
VD11AURA <hallo64789@gmail.com>
2025-06-17 21:01:40 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'public/admin', dest: '' }
+      ]
+    })
+  ]
 })
-
